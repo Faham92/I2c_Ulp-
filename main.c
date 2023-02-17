885a11088908293f705e650d6e652cf5da04fc65
Faham92 <118739668+Faham92@users.noreply.github.com>
2023-02-17 11:09:12 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,156 +1,145 @@
-
-/* ULP I2C bit bang BMP-180 Example
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <stdio.h>
-#include <math.h>
 #include "esp_sleep.h"
-#include "nvs.h"
-#include "nvs_flash.h"
-#include "soc/rtc_cntl_reg.h"
-#include "soc/rtc_io_reg.h"
-#include "soc/sens_reg.h"
-#include "soc/soc.h"
-#include "driver/gpio.h"
+#include "sdkconfig.h"
 #include "driver/rtc_io.h"
 #include "esp32/ulp.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "sdkconfig.h"
+#include "esp_log.h"
+#include "soc/soc.h"
+#include "soc/sens_reg.h"
+#include "soc/rtc_io_reg.h"
 #include "soc/soc_ulp.h"
+#include "soc/rtc_i2c_reg.h"
+#include "soc/rtc_io_periph.h"
+
 #include "ulp_main.h"
 
+
+// Unlike the esp-idf always use these binary blob names
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
 extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
 
+typedef struct {
+    uint32_t scl_low;       /*!< FAST_CLK cycles for SCL to be low [19b] */
+    uint32_t scl_high;      /*!< FAST_CLK cycles for SCL to be high [20b] */
+    uint32_t sda_duty;      /*!< FAST_CLK cycles SDA will switch after falling edge of SCL [20b] */
+    uint32_t scl_start;     /*!< FAST_CLK cycles to wait before generating start condition [20b] */
+    uint32_t scl_stop;      /*!< FAST_CLK cycles to wait before generating stop condition [20b] */
+    uint32_t timeout;       /*!< Maximum number of FAST_CLK cycles that the transmission can take [20b] */
+    bool scl_pushpull;      /*!< SCL is push-pull (true) or open-drain (false) */
+    bool sda_pushpull;      /*!< SDA is push-pull (true) or open-drain (false) */
+    bool rx_lsbfirst;       /*!< Receive LSB first */
+    bool tx_lsbfirst;       /*!< Send LSB first */
+}hulp_i2c_controller_config_t;
 
-const gpio_num_t gpio_led = GPIO_NUM_2;
-const gpio_num_t gpio_scl = GPIO_NUM_32;
-const gpio_num_t gpio_sda = GPIO_NUM_33;
-//const gpio_num_t gpio_builtin = GPIO_NUM_22;
+void hulp_configure_i2c_pins(gpio_num_t , gpio_num_t );
+esp_err_t hulp_configure_i2c_controller( hulp_i2c_controller_config_t* );
+void register_i2c_slave(uint8_t );
+static void start_ulp_program();
+void ini_config(hulp_i2c_controller_config_t *);
 
+RTC_DATA_ATTR hulp_i2c_controller_config_t* config;
+gpio_num_t sda_pin = 4;
+gpio_num_t scl_pin = 0;
 
-static void init_ulp_program()
+void app_main() 
 {
-    rtc_gpio_init(gpio_led);
-    rtc_gpio_set_direction(gpio_led, RTC_GPIO_MODE_OUTPUT_ONLY);
-
-    rtc_gpio_init(gpio_scl);
-    rtc_gpio_set_direction(gpio_scl, RTC_GPIO_MODE_INPUT_ONLY);
-    rtc_gpio_init(gpio_sda);
-    rtc_gpio_set_direction(gpio_sda, RTC_GPIO_MODE_INPUT_ONLY);
-
-    esp_err_t err = ulp_load_binary(0, ulp_main_bin_start,
-            (ulp_main_bin_end - ulp_main_bin_start) / sizeof(uint32_t));
-    ESP_ERROR_CHECK(err);
-
-    /* Set ULP wake up period to T = 1000ms
-     * Minimum pulse width has to be T * (ulp_debounce_counter + 1) = 80ms.
-     */
-    REG_SET_FIELD(SENS_ULP_CP_SLEEP_CYC0_REG, SENS_SLEEP_CYCLES_S0, 150000);
-
+  esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
+  esp_sleep_enable_ulp_wakeup();                       //Prévient le microcontroleur qu'il peut etre reveille par l'ulp
+  if (cause != ESP_SLEEP_WAKEUP_ULP)
+  {
+    hulp_configure_i2c_pins(scl_pin, sda_pin);
+    ini_config(config);
+    hulp_configure_i2c_controller(config);
+    start_ulp_program();
+  }
+  ulp_run((&ulp_entry-RTC_SLOW_MEM));
+  register_i2c_slave(0x77);
+ 
 }
 
 
-#define ac1 ((short)ulp_ac1)
-#define ac2 ((short)ulp_ac2)
-#define ac3 ((short)ulp_ac3)
-#define ac4 ((uint16_t)ulp_ac4)
-#define ac5 ((uint16_t)ulp_ac5)
-#define ac6 ((uint16_t)ulp_ac6)
-#define b1 ((short)ulp_b1)
-#define b2 ((short)ulp_b2)
-#define mb ((short)ulp_mb)
-#define mc ((short)ulp_mc)
-#define md ((short)ulp_md)
-
-int computeB5(int32_t UT) {
-  int32_t X1 = (UT - (int32_t)ac6) * ((int32_t)ac5) >> 15;
-  int32_t X2 = ((int32_t)mc << 11) / (X1+(int32_t)md);
-  return X1 + X2;
+// Use this function for all your first time init like setup() used to be
+/*static void init_run_ulp() 
+{
+  
+}
+*/
+static void start_ulp_program()
+{
+    vTaskDelay(100/portTICK_PERIOD_MS);
+    ulp_load_binary(0, ulp_main_bin_start, (ulp_main_bin_end - ulp_main_bin_start) / sizeof(uint32_t));
 }
 
-static void print_status()
+void hulp_configure_i2c_pins(gpio_num_t scl_pin, gpio_num_t sda_pin)
 {
-//    printf("%d ", ac1);
-//    printf("%d ", ac2);
-//    printf("%d ", ac3);
-//    printf("%d ", ac4);
-//    printf("%d ", ac5);
-//    printf("%d\n", ac6);
-//    printf("%d ", b1);
-//    printf("%d ", b2);
-//    printf("%d ", mb);
-//    printf("%d ", mc);
-//    printf("%d ", md);
-//    printf("\n");
+  /*Initialisation des pins RTC*/
+    rtc_gpio_init(sda_pin);
+    rtc_gpio_init(scl_pin);
+    rtc_gpio_set_direction(sda_pin,RTC_GPIO_MODE_INPUT_OUTPUT);
+    rtc_gpio_set_direction(scl_pin,RTC_GPIO_MODE_INPUT_OUTPUT);
 
-    printf("rawT: %d Address: 0x%04x counter: %d\n", ulp_temp & 0xFFFF, ulp_temp >> 19, ulp_counter & 0xFFFF);
-    printf("rawP1/2: %d %d\n", ulp_pressure & 0xFFFF, ulp_pressure2 & 0xFFFF);
+    const int scl_rtcio_num = rtc_io_number_get(scl_pin);
+    SET_PERI_REG_BITS(rtc_io_desc[scl_rtcio_num].reg, RTC_IO_TOUCH_PAD1_FUN_SEL_V, 0x3, rtc_io_desc[scl_rtcio_num].func);
+    const int sda_rtcio_num = rtc_io_number_get(sda_pin);
+    SET_PERI_REG_BITS(rtc_io_desc[sda_rtcio_num].reg, RTC_IO_TOUCH_PAD1_FUN_SEL_V, 0x3, rtc_io_desc[sda_rtcio_num].func);
 
-    int oversampling= 1;
-
-    int32_t UP= ((((ulp_pressure & 0xFFFF) << 8) | (ulp_pressure2 & 0xFFFF)) >> (8-oversampling));
-    int32_t B3, B5, B6, X1, X2, X3, p;
-    uint32_t B4, B7;
-
-    float temp;
-    B5 = computeB5(ulp_temp & 0xFFFF);
-    temp = (B5+8) / 16.0;
-    temp /= 10;
-    printf("\nTemperature: %.2f\n", temp);
-
-    // do pressure calcs
-    B6 = B5 - 4000;
-    X1 = ((int32_t)b2 * ( (B6 * B6)>>12 )) >> 11;
-    X2 = ((int32_t)ac2 * B6) >> 11;
-    X3 = X1 + X2;
-    B3 = ((((int32_t)ac1*4 + X3) << oversampling) + 2) / 4;
-
-    X1 = ((int32_t)ac3 * B6) >> 13;
-    X2 = ((int32_t)b1 * ((B6 * B6) >> 12)) >> 16;
-    X3 = ((X1 + X2) + 2) >> 2;
-    B4 = ((uint32_t)ac4 * (uint32_t)(X3 + 32768)) >> 15;
-    B7 = ((uint32_t)UP - B3) * (uint32_t)( 50000UL >> oversampling );
-
-    if (B7 < 0x80000000) {
-        p = (B7 * 2) / B4;
-    } else {
-        p = (B7 / B4) * 2;
-    }
-    X1 = (p >> 8) * (p >> 8);
-    X1 = (X1 * 3038) >> 16;
-    X2 = (-7357 * p) >> 16;
-
-    p = p + ((X1 + X2 + (int32_t)3791)>>4);
-    float pressure = p, altitude_meters= 3;
-    printf("Pressure: %.2f\n\n", (pressure / pow(1.0-altitude_meters/44330, 5.255))/100.0);
+    REG_SET_FIELD(RTC_IO_SAR_I2C_IO_REG, RTC_IO_SAR_I2C_SCL_SEL, 0); // Touch pad 0 GPIO4 RTC_GPIO10
+    REG_SET_FIELD(RTC_IO_SAR_I2C_IO_REG, RTC_IO_SAR_I2C_SDA_SEL, 0);// Touch pad 1  GPIO0 RTC_GPIO11
 }
+ 
 
-void app_main()
+esp_err_t hulp_configure_i2c_controller(hulp_i2c_controller_config_t *config)
 {
-    esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
-    if (cause != ESP_SLEEP_WAKEUP_ULP) {
-        printf("Not ULP wakeup, initializing ULP\n");
-        init_ulp_program();
-    } else {
-
-    	printf("ULP wakeup, printing status\n");
-        print_status();
+    if(!config)
+    {
+        return ESP_ERR_INVALID_ARG;
     }
 
-    printf("Entering deep sleep\n\n");
+    if(
+        (config->scl_low > RTC_I2C_SCL_LOW_PERIOD_V) ||
+        (config->scl_high > RTC_I2C_SCL_HIGH_PERIOD_V) ||
+        (config->sda_duty > RTC_I2C_SDA_DUTY_V) ||
+        (config->scl_start > RTC_I2C_SCL_START_PERIOD_V) ||
+        (config->scl_stop > RTC_I2C_SCL_STOP_PERIOD_V) ||
+        (config->timeout > RTC_I2C_TIMEOUT_V)
+      )
+    {
+        return ESP_ERR_INVALID_ARG;
+    }
 
-    ESP_ERROR_CHECK( esp_sleep_enable_ulp_wakeup() );
+    REG_SET_FIELD(RTC_I2C_CTRL_REG, RTC_I2C_RX_LSB_FIRST, config->rx_lsbfirst);
+    REG_SET_FIELD(RTC_I2C_CTRL_REG, RTC_I2C_TX_LSB_FIRST, config->tx_lsbfirst);
+    REG_SET_FIELD(RTC_I2C_CTRL_REG, RTC_I2C_SCL_FORCE_OUT, config->scl_pushpull);
+    REG_SET_FIELD(RTC_I2C_CTRL_REG, RTC_I2C_SDA_FORCE_OUT, config->sda_pushpull);
 
-    /* Start the program */
-    esp_err_t err = ulp_run(&ulp_entry - RTC_SLOW_MEM);
-    ESP_ERROR_CHECK(err);
-
-    esp_deep_sleep_start();
+    REG_SET_FIELD(RTC_I2C_SCL_LOW_PERIOD_REG, RTC_I2C_SCL_LOW_PERIOD, config->scl_low);
+    REG_SET_FIELD(RTC_I2C_SCL_HIGH_PERIOD_REG, RTC_I2C_SCL_HIGH_PERIOD, config->scl_high);
+    REG_SET_FIELD(RTC_I2C_SDA_DUTY_REG, RTC_I2C_SDA_DUTY, config->sda_duty);
+    REG_SET_FIELD(RTC_I2C_SCL_START_PERIOD_REG, RTC_I2C_SCL_START_PERIOD, config->scl_start);
+    REG_SET_FIELD(RTC_I2C_SCL_STOP_PERIOD_REG, RTC_I2C_SCL_STOP_PERIOD, config->scl_stop);
+    REG_SET_FIELD(RTC_I2C_TIMEOUT_REG, RTC_I2C_TIMEOUT, config->timeout);
+    REG_SET_FIELD(RTC_I2C_CTRL_REG, RTC_I2C_MS_MODE, 1);
+    return ESP_OK;
 }
 
+void register_i2c_slave(uint8_t reg)
+{
+  REG_SET_FIELD(SENS_SAR_SLAVE_ADDR1_REG,SENS_I2C_SLAVE_ADDR0, reg);
+}
+
+void ini_config(hulp_i2c_controller_config_t *config)
+{
+    
+    config->scl_low = 40;                //initialisation fait à la page 664 de la technical reference de l'esp32            
+    config->scl_high = 40;                         
+    config->sda_duty = 16;                         
+    config->scl_start = 30;                        
+    config->scl_stop = 44;                         
+    config->timeout = 200;                         
+    config->scl_pushpull = false;                 
+    config->sda_pushpull = false;                 
+    config->rx_lsbfirst = false;                  
+    config->tx_lsbfirst = false;
+}
